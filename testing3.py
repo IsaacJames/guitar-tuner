c5dc35613fb9fb0e1b32d2f860059406acfdf64f
Isaac <isaac.james@hotmail.co.uk>
2016-09-29 21:06:13 +0100

    Autocorrelation in progress

--- testing3.py
+++ testing3.py
@@ -1,36 +1,57 @@
 import sounddevice as sd
 import matplotlib.pyplot as plt
-import numpy as np
 
 CHANNELS = 1
 RATE = 8000
-RECORD_SECONDS = 1
+RECORD_SECONDS = 0.5
 sd.default.samplerate = RATE
 
 print("Ready")
 
+
 while True:
+
+    # Waits for enter key to be pressed
     input()
+
     print("Recording")
 
-    myrecording = sd.rec(int(RECORD_SECONDS * RATE), samplerate=RATE,
-                         channels=CHANNELS, blocking=True, dtype='float64')
-    print("*")
+    recording = sd.rec(int(RECORD_SECONDS * RATE), samplerate=RATE, channels=CHANNELS, blocking=True, dtype='float64')
+    print("Finished")
 
-    amplitudes = [item for sublist in myrecording for item in sublist]
+    # Converts recording into 1D array
+    amplitudes = [item for sublist in recording for item in sublist]
 
+    # Finds frequency from zero crossings
+    def zero_crossings():
 
-    prev = amplitudes[0]
-    count = 0
+        prev = amplitudes[0]
+        crossings = 0
 
-    for num in amplitudes:
-        if num == 0 and prev != 0:
-            count += 1
-        if prev * num < 0:
-            count += 1
-        prev = num
+        for cur in amplitudes:
+            if cur == 0 and prev != 0:
+                crossings += 1
+            if prev * cur < 0:
+                crossings += 1
+            prev = cur
 
-    print("Zero crossings: ", count // 2)
+        return RECORD_SECONDS / (crossings - 1//2)
 
-    windowed = amplitudes *
-    #plt.plot(np.fft.rfft(amplitudes))
+    # Finds
+    correlations = []
+
+    for i in range(0, len(amplitudes)):
+        cor = 0
+        for k in range(0, len(amplitudes) - i):
+            cor += (amplitudes[k]) * (amplitudes[k + i])
+            correlations.append(cor)
+
+    fig = plt.figure()
+
+    ax1 = fig.add_subplot(211)
+    ax1.plot(amplitudes)
+
+    ax2 = fig.add_subplot(212)
+    ax2.plot(correlations)
+
+    plt.show()
--- testing4.py
+++ testing4.py
@@ -1,3 +1,4 @@
-while True:
-    input("Press")
-    print("*")
+for i in range(0, len(amplitudes)):
+    sum = 0
+    for k in range(0, len(amplitudes) - i):
+        sum += (amplitudes[k]) * (amplitudes[k + i] / 256)
