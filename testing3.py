7f91c6b988e4c97367ca3a7afcddc578594229d2
Isaac-Surface <isaac.james@hotmail.co.uk>
2016-10-01 17:35:50 +0100

    Functions added.

--- testing3.py
+++ testing3.py
@@ -9,6 +9,36 @@ sd.default.samplerate = RATE
 print("Ready")
 
 
+# Finds frequency from zero crossings
+def zero_crossings(signal):
+    prev = signal[0]
+    crossings = 0
+
+    for cur in signal:
+        if cur == 0 and prev != 0:
+            crossings += 1
+        if prev * cur < 0:
+            crossings += 1
+        prev = cur
+
+    return RECORD_SECONDS / (crossings - 1 // 2)
+
+
+# Finds frequency via autocorrelation
+def autocorrelation(signal):
+    correlations = []
+
+    for i in range(0, len(signal)):
+        cor = 0
+        for k in range(0, len(signal) - i):
+            cor += (signal[k]) * (signal[k + i])
+            correlations.append(cor)
+
+    # Plots correlation coefficients
+    ax2 = fig.add_subplot(212)
+    ax2.plot(correlations)
+
+
 while True:
 
     # Waits for enter key to be pressed
@@ -16,42 +46,20 @@ while True:
 
     print("Recording")
 
+    # Generates 2D array from microphone audio
     recording = sd.rec(int(RECORD_SECONDS * RATE), samplerate=RATE, channels=CHANNELS, blocking=True, dtype='float64')
+
     print("Finished")
 
     # Converts recording into 1D array
-    amplitudes = [item for sublist in recording for item in sublist]
-
-    # Finds frequency from zero crossings
-    def zero_crossings():
-
-        prev = amplitudes[0]
-        crossings = 0
-
-        for cur in amplitudes:
-            if cur == 0 and prev != 0:
-                crossings += 1
-            if prev * cur < 0:
-                crossings += 1
-            prev = cur
-
-        return RECORD_SECONDS / (crossings - 1//2)
-
-    # Finds
-    correlations = []
-
-    for i in range(0, len(amplitudes)):
-        cor = 0
-        for k in range(0, len(amplitudes) - i):
-            cor += (amplitudes[k]) * (amplitudes[k + i])
-            correlations.append(cor)
+    buffer = [item for sublist in recording for item in sublist]
 
     fig = plt.figure()
 
-    ax1 = fig.add_subplot(211)
-    ax1.plot(amplitudes)
+    autocorrelation(buffer)
 
-    ax2 = fig.add_subplot(212)
-    ax2.plot(correlations)
+    # Plots unprocessed signal
+    amplitudes = fig.add_subplot(211)
+    amplitudes.plot(buffer)
 
     plt.show()
