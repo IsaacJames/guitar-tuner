6466d30cf23253e41b722e8bde1c5ca60735ca61
Isaac <isaac.james@hotmail.co.uk>
2016-10-02 19:13:14 +0100

    Transfer to Pi.

--- mechanical_tuner.py
+++ mechanical_tuner.py
@@ -1,4 +1,13 @@
 import sounddevice as sd
+import numpy as np
+import time
+import RPi.GPIO as GPIO
+
+# Sets up GPIO pins for use
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(17, GPIO.OUT)
+GPIO.setup(18, GPIO.OUT)
 
 # Rate and recording time can be changed to vary accuracy of frequency detection
 CHANNELS = 1
@@ -6,57 +15,91 @@ RATE = 8000
 RECORD_SECONDS = 0.1
 sd.default.samplerate = RATE
 
+std_tuning = [82.41, 110, 146.8, 196, 246.9, 329.6]
+
 print("Ready")
 
 
 # Finds frequency from zero crossings
 def zero_crossings(signal):
-    prev = signal[0]
+    prev_cor = signal[0]
     crossings = 0
 
     for cur in signal:
-        if cur == 0 and prev != 0:
+        if cur == 0 and prev_cor != 0:
             crossings += 1
-        if prev * cur < 0:
+        if prev_cor * cur < 0:
             crossings += 1
-        prev = cur
+        prev_cor = cur
 
     return RECORD_SECONDS, crossings, (RECORD_SECONDS / (crossings - 1 // 2))
 
 
 # Finds frequency via autocorrelation
 def autocorrelation(signal):
-
     correlations = []
-    prev = 0
+    prev_cor = 0
+
+    peak = 1
 
     for i in range(0, len(signal)):
         cor = 0
         for k in range(0, len(signal) - i):
             cor += (signal[k]) * (signal[k + i])
         correlations.append(cor)
-        
-        if i > 10 and prev > 0.8 * correlations[0] and cor < prev:
+
+        if i > 10 and prev_cor > 0.8 * correlations[0] and cor < prev_cor:
             peak = i - 1
             break
-        
-        prev = cor
 
-    return 1 / (peak * (RECORD_SECONDS / (RATE * RECORD_SECONDS))), correlations[peak] / correlations[0]
+        prev_cor = cor
+
+    if peak == 1 or peak == 10:
+        return
+
+    else:
+        return 1 / (peak * (RECORD_SECONDS / (RATE * RECORD_SECONDS)))
+
 
 while True:
 
-    # Waits for enter key to be pressed
-    input()
-
-    print("Recording")
+    #print("Recording")
 
     # Generates 2D array from microphone audio
     recording = sd.rec(int(RECORD_SECONDS * RATE), samplerate=RATE, channels=CHANNELS, blocking=True, dtype='float64')
 
-    print("Finished")
+    #print("Finished")
 
     # Converts recording into 1D array
     buffer = [item for sublist in recording for item in sublist]
+    abs_amp = [abs(num) for num in buffer]
+    avg_abs_amp = np.average(abs_amp)
 
-    print(autocorrelation(buffer))
+    if avg_abs_amp > 0.001:
+
+        recorded_frequency = autocorrelation(buffer)
+
+        prev_diff = 999
+
+        for frequency in std_tuning:
+            diff = abs(frequency - recorded_frequency)
+            if diff < prev_diff:
+                target = frequency
+                final_diff = diff
+            prev_diff = diff
+
+        if final_diff < 5:
+            print('tuned!')
+            exit()
+
+        if recorded_frequency < target:
+            GPIO.output(17, True)
+            GPIO.output(18, False)
+            time.sleep(1)
+        else:
+            GPIO.output(17, False)
+            GPIO.output(18, True)
+            time.sleep(1)
+
+        GPIO.output(17, False)
+        GPIO.output(18, False)
--- testing.py
+++ testing.py
@@ -1,4 +1,4 @@
-import pyaudio
+'''import pyaudio
 
 CHUNK = 2024
 WIDTH = 2
@@ -24,4 +24,73 @@ while True:
 stream.stop_stream()
 stream.close()
 
-p.terminate()
+p.terminate()'''
+
+import sounddevice as sd
+import numpy as np
+
+# Rate and recording time can be changed to vary accuracy of frequency detection
+CHANNELS = 1
+RATE = 8000
+RECORD_SECONDS = 0.1
+sd.default.samplerate = RATE
+
+print("Ready")
+
+
+# Finds frequency from zero crossings
+def zero_crossings(signal):
+    prev = signal[0]
+    crossings = 0
+
+    for cur in signal:
+        if cur == 0 and prev != 0:
+            crossings += 1
+        if prev * cur < 0:
+            crossings += 1
+        prev = cur
+
+    return RECORD_SECONDS, crossings, (RECORD_SECONDS / (crossings - 1 // 2))
+
+
+# Finds frequency via autocorrelation
+def autocorrelation(signal):
+    correlations = []
+    prev = 0
+
+    peak = 1
+
+    for i in range(0, len(signal)):
+        cor = 0
+        for k in range(0, len(signal) - i):
+            cor += (signal[k]) * (signal[k + i])
+        correlations.append(cor)
+
+        if i > 10 and prev > 0.8 * correlations[0] and cor < prev:
+            peak = i - 1
+            break
+
+        prev = cor
+
+    if peak == 1 or peak == 10:
+        return
+
+    else:
+        return 1 / (peak * (RECORD_SECONDS / (RATE * RECORD_SECONDS))), correlations[peak] / correlations[0]
+
+
+while True:
+
+    #print("Recording")
+
+    # Generates 2D array from microphone audio
+    recording = sd.rec(int(RECORD_SECONDS * RATE), samplerate=RATE, channels=CHANNELS, blocking=True, dtype='float64')
+
+    #print("Finished")
+
+    # Converts recording into 1D array
+    buffer = [item for sublist in recording for item in sublist]
+    avg_abs_amp = [abs(num) for num in buffer]
+
+    if np.average(avg_abs_amp) > 0.001:
+        print(autocorrelation(buffer), np.average(avg_abs_amp))
